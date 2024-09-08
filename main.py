34c523825c315bc72da146b2861fa27a2210fd56
sapiaf <123062954+sapiaf@users.noreply.github.com>
2024-09-08 18:46:28 +0200

    Created player

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
-       screen.fill((0, 0, 0))
-       pygame.display.flip()
-
-       for event in pygame.event.get():
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
 
 # This line ensures the main() function is only called when this file is run directly; 
 # it won't run if it's imported as a module.
